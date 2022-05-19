944eb111a1d8bcc4c571f6a90d2ae0c1e04be258
Victor Albenor <victoralbenor@gmail.com>
2022-05-19 15:10:05 -0300

    Clean up GameConstants

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -7,17 +7,11 @@ public class GameConstants
     public const string k_MouseAxisNameHorizontal = "Mouse X";
     public const string k_AxisNameJoystickLookVertical = "Look Y";
     public const string k_AxisNameJoystickLookHorizontal = "Look X";
-    public const string k_ButtonNameJump = "Jump";
     public const string k_ButtonNameFire = "Fire";
     public const string k_ButtonNameGamepadFire = "Gamepad Fire";
-    public const string k_ButtonNameSprint = "Sprint";
     public const string k_ButtonNameCrouch = "Crouch";
-    public const string k_ButtonNameAim = "Aim";
-    public const string k_ButtonNameGamepadAim = "Gamepad Aim";
-    public const string k_ButtonNameSwitchWeapon = "Mouse ScrollWheel";
-    public const string k_ButtonNameGamepadSwitchWeapon = "Gamepad Switch";
-    public const string k_ButtonNameNextWeapon = "NextWeapon";
-    public const string k_ButtonNamePauseMenu = "Pause Menu";
+    public const string k_ButtonNameAlternativeFire = "Alternative Fire"
+    public const string k_ButtonNameGamepadAlternativeFire = "Gamepad Alternative Fire"
     public const string k_ButtonNameSubmit = "Submit";
     public const string k_ButtonNameCancel = "Cancel";
 }
